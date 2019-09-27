ce8908d9c1687033a9d4362881e04381bcf00646
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2019-09-27 21:32:28 +0600

    website completed, animations added, responsive

--- main.js
+++ main.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
     $(this).toggleClass("open");
     $(".top-nav").toggleClass("open");
   });
+
+  $(".top-nav .nav-link").on("click", function() {
+    $(".menu-toggler").removeClass("open");
+    $(".top-nav").removeClass("open");
+  });
+
+  AOS.init({
+    easing: "ease",
+    duration: 1800,
+    once: true
+  });
 });
